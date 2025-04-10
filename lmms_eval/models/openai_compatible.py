9ac212e1b7aaaa017eb4415d9908da3a29367212
Erlie Shang <ershang-oss@fireworks.ai>
2025-04-10 06:48:32 +0000

    replace max_output_tokens with max_completion_tokens

--- lmms_eval/models/openai_compatible.py
+++ lmms_eval/models/openai_compatible.py
@@ -186,7 +186,7 @@ class OpenAICompatible(lmms):
             if "num_beams" not in gen_kwargs:
                 gen_kwargs["num_beams"] = 1
 
-            payload["max_output_tokens"] = gen_kwargs["max_new_tokens"]
+            payload["max_completion_tokens"] = gen_kwargs["max_new_tokens"]
             payload["temperature"] = gen_kwargs["temperature"]
 
             if "o1" in self.model_version or "o3" in self.model_version:
